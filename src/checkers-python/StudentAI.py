a93475a314efd5e62ec31f4390615156949b8a99
Victor Guan <vguan12@uci.edu>
2022-11-25 23:22:18 -0800

    first working version

--- src/checkers-python/StudentAI.py
+++ src/checkers-python/StudentAI.py
@@ -1,58 +1,148 @@
 from random import randint
 from BoardClasses import Move
 from BoardClasses import Board
-#The following part should be completed by students.
-#Students can modify anything except the class name and existing functions and variables.
+# The following part should be completed by students.
+# Students can modify anything except the class name and existing functions and variables.
 
-#from datetime import datetime, timedelta
+from datetime import datetime, timedelta
+import copy, math
+
 
 class StudentAI():
-
-    def __init__(self,col,row,p):
+    def __init__(self, col, row, p):
         self.col = col
         self.row = row
         self.p = p
-        self.board = Board(col,row,p)
+        self.board = Board(col, row, p)
         self.board.initialize_game()
         self.color = ''
-        self.opponent = {1:2,2:1}
+        self.opponent = {1: 2, 2: 1}
         self.color = 2
-    def get_move(self,move):
+
+    # makes a random move
+    def make_sim_move(self, board, color):
+        """
+        Simulates a random move. Used in StudentAI.simulate().
+        @board : Board object, where move is made
+        @color : Current color that makes move
+        """
+        moves = board.get_all_possible_moves(color)
+        if len(moves) > 0:
+            index = randint(0, len(moves) - 1)
+            inner_index = randint(0, len(moves[index]) - 1)
+            move = moves[index][inner_index]
+            board.make_move(move, color)
+        return board
+
+    def simulate(self, board, color, allottedtime, tie_counter=40):
+        """
+        Simulates a game based on provided board state and how much time it can run at maximum
+        @return : True if win, False if not
+        """
+
+        turns_to_tie = 0
+        simulatestarttime = datetime.now()
+
+        while datetime.now() - simulatestarttime < allottedtime:
+            board = self.make_sim_move(board, color)
+
+            # returns True if self won, False if opponent won
+            if 0 != board.is_win(color):
+                return self.color == board.is_win(color)
+            # will win if
+            if turns_to_tie >= tie_counter:
+                return False
+
+            # change color/turn
+            if color == self.color:
+                color = self.opponent[self.color]
+            else:
+                color = self.color
+            turns_to_tie += 1
+        return False
+
+    def get_move(self, move):
         starttime = datetime.now()
+        if len(move) != 0:
+            self.board.make_move(move, self.opponent[self.color])
+        else:
+            self.color = 1
+
         tree = self.board.get_all_possible_moves(self.color)
-        chosen = tree[0][0]
-        currentpiece,currentmove = 0,0
-        while (datetime.now() - starttime < timedelta(minutes=11,seconds=59)):
-        #    currentmax = 0
-        #     result = simulate(child)
-        #     if result == win
-        #         wincount++
-        #     uct = wincount/nodetotal + sqrt(2*ln(parenttotal)/nodetotal)
-        #     if uct > currentmax
-        #         chosen = child
-        #         currentmax = uct
-        #     if len(tree[currentpieceindex])
-        self.board.make_move(chosen,self.color)
+        currentmove = tree[0][0]  # the first selects the first possible move available
+        chosen = currentmove  # the default move to return is the first possible move available
+        resultdict = {}  # holds known moves and their associated win ratios
+        resultdict[currentmove] = [0, 0]  # instantiates resultdict with values for first move
+        moveboard = copy.deepcopy(
+            self.board)  # copy of the board, used to simulate games without altering actual board state
+        result = False  # records latest simulation result (win or not)
+        parenttotal = 0
+
+        state = 2
+        # state is used to segment aspects of the mcts algorithm into smaller parts, so the while loop condition can be checked more often and the risk of exceeding the allotted time limit is mitigated
+        """state key
+        0 := select next node to test
+        1 := expand selected node & choose
+        2 := simulate game off of move
+        3 := backpropagate results
+        """
+
+        while (datetime.now() - starttime < timedelta(seconds=11, milliseconds=900)):
+            if state == 0:
+                maxknown = -1
+                for childpiece in tree:
+                    for childmove in childpiece:
+                        if childmove not in resultdict or resultdict[childmove][1] <= 0:
+                            currentmove = childmove
+                            resultdict[currentmove] = [0, 0]
+                            break
+                        else:
+                            uct = resultdict[childmove][0] / resultdict[childmove][1] + math.sqrt(
+                                2 * math.log(parenttotal) / resultdict[childmove][1])
+                            # uct = nodewins/nodetotal + sqrt(2*ln(parenttotal)/nodetotal)
+                            if uct > maxknown:
+                                currentmove = childmove
+                                maxknown = uct
+                state = 1
+            if state == 1:
+                moveboard = copy.deepcopy(self.board)  # copy of the board
+                moveboard.make_move(currentmove, self.color)  # update simulation board with chosen move
+                if self.color == moveboard.is_win(self.color):
+                    self.board.make_move(currentmove, self.color)
+                    return currentmove
+                state = 2
+            if state == 2:
+                result = self.simulate(moveboard, self.opponent[self.color],
+                                       timedelta(seconds=11, milliseconds=900) - (datetime.now() - starttime),
+                                       tie_counter=40)  # simulate game off of focus move
+                state = 3
+            elif state == 3:
+                if result:
+                    resultdict[currentmove][0] += 1
+                resultdict[currentmove][1] += 1
+                parenttotal += 1
+                state = 0
+            else:
+                state = 0
+        bestodds = 0
+        for piece in tree:
+            for move in piece:
+                if resultdict[move][0] / resultdict[move][1] > bestodds:
+                    chosen = move
+                    bestodds = resultdict[move][0] / resultdict[move][1]
+        self.board.make_move(chosen, self.color)
         return chosen
 
-    #first run ~ selection is start state of board when running get_move()
+    # first run
+    # selection is start state of board when running get_move()
     # expansion is get_all_possible_moves from state
     # simulation tests one of the possible moves
-    # result is backpropped, adds to total games regardless and win count if win
+    # backpropagate result, adds to total games regardless and win count if win
+    # consecutive runs
     # selection chooses next untouched move
     # expansion same
-    # backprop same
+    # simulation same
+    # backpropagate same
     # ...
     # once all base level moves checked, move down to moves branching off of main possible moves
-
-            ### below is default for get_move
-        #if len(move) != 0:
-        #    self.board.make_move(move,self.opponent[self.color])
-        #else:
-        #    self.color = 1
-        #moves = self.board.get_all_possible_moves(self.color)
-        #index = randint(0,len(moves)-1)
-        #inner_index =  randint(0,len(moves[index])-1)
-        #move = moves[index][inner_index]
-        #self.board.make_move(move,self.color)
-        #return move
+    # run against RandomAI with "python3 AI_Runner.py 8 8 3 l /home/agee4/CheckersAI/src/checkers-python/main.py /home/agee4/CheckersAI/src/checkers-python/Tools/Sample_AIs/Random_AI/main.py"
